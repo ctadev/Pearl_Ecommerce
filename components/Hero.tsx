0856f84990057cdaa2ac3a8e0bcb19e343edf85c
ctadev <ctakammy@gmail.com>
2023-03-03 03:23:31 -0500

    test

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -32,9 +32,9 @@ const s = {
   hero_container:
     "sticky .stickySafari z-0 top-0 mx-auto flex h-screen max-w-[1350px] items-center justify-between px-8",
   h1: "space-y-3 text-5xl font-semibold tracking-wide lg:text-6xl xl:text-7xl",
-  span: "block bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent",
+  span: "block bg-purple-500 bg-clip-text text-transparent",
   buy_container: "flex items-center gap-8",
-  btn: "bg-gradient-to-r from-violet-500 to-pink-500 px-8 py-4 text-md text-white font-bold rounded-md",
+  btn: "bg-pink-500 px-8 py-4 text-md text-white font-bold rounded-md",
   learn: "",
   hero_image:
     "relative hidden h-[450px] w-[450px] transition-all duration-500 md:inline lg:h-[650px] lg:w-[650px]",
